e3a112425bb171aaca17853d5d69902da6322f8f
LeoMassonETPA <l.masson@rns.ecolescreatives.com>
2020-12-22 17:12:43 +0100

    Version1.0

---
+++ js/script.js
@@ -0,0 +1,159 @@
+var atq_joueur= 24;
+var atq_monstre=12;
+var vie_monstre = 175;
+var vie_joueur= 100;
+var mana_joueur = 100;
+var def_joueur = 25;
+var cout_mana = 25;
+var poison = 0;
+var dgt_poison= 13;
+var Hp_perso1=document.getElementById("Hp_perso1");
+var Mana_perso1=document.getElementById("Mana_perso1");
+var Def_perso1=document.getElementById("Def_perso1");
+var InfobulleNom=document.getElementById("InfobulleNom");
+var InfobulleVie=document.getElementById("InfobulleVie");
+var Image_boss=document.getElementById("Image_boss");
+var Bouton_attaquer=document.getElementById("atq");
+var Bouton_defense=document.getElementById("shield");
+var Bouton_special=document.getElementById("spe");
+var Boite_dialogue=document.getElementById("Boite_dialogue");
+var Action_bouton=document.getElementById("Action_bouton");
+var Atq_Disponible=true;
+var Def_Disponible=true;
+var Spe_Disponible=true;
+
+Hp_perso1.innerHTML=vie_joueur;
+Mana_perso1.innerHTML=mana_joueur;
+Def_perso1.innerHTML=def_joueur;
+InfobulleNom.innerHTML="Reshor l'évadé";
+InfobulleVie.innerHTML=vie_monstre + "HP";
+
+Image_boss.onmouseover=function(){
+	InfobulleNom.style.opacity="1";
+	InfobulleVie.style.opacity="1";
+	Image_boss.onmouseout=function(){
+		InfobulleNom.style.opacity="0";
+		InfobulleVie.style.opacity="0";	
+	}
+}
+
+
+function Condition_Victoire(){
+	if (vie_monstre <=0){
+		Boite_dialogue.innerHTML="Félicitation, vous avez gagné";
+		Image_boss.style.backgroundImage= "url()";
+		Action_bouton.style.visibility= "hidden";
+	}
+}
+
+function Condition_Defaite(){
+	if (vie_joueur <=0){
+	Boite_dialogue.innerHTML="Dommage, c'est perdu !";
+	Action_bouton.style.visibility= "hidden";
+	}
+}
+
+function Atq_Monstre(){
+	vie_joueur -= atq_monstre;
+	Boite_dialogue.innerHTML="Le monstre vous attaque et vous inflige " + atq_monstre + " points de dégâts";
+	Hp_perso1.innerHTML= vie_joueur;
+}
+
+
+function Attaquer(){
+	if (Atq_Disponible==true){
+		Atq_Disponible=false;
+		Bouton_attaquer.style.backgroundColor="#5B4C49";
+		Def_Disponible=true;
+		Bouton_defense.style.backgroundColor="#DB3613";
+		if (mana_joueur>24){
+			Spe_Disponible=true;
+			Bouton_special.style.backgroundColor="#DB3613";
+		}
+		vie_monstre -= atq_joueur;
+		InfobulleVie.innerHTML=vie_monstre + "HP";
+		Boite_dialogue.innerHTML="Vous infligez " + atq_joueur + " points de dégâts au monstre";
+		if (poison>0){
+			vie_monstre -= dgt_poison;
+		}
+	}
+	
+}
+
+function Defense(){
+	if (Def_Disponible==true){
+		Atq_Disponible=true;
+		Bouton_attaquer.style.backgroundColor="#DB3613";
+		Def_Disponible=false;
+		Bouton_defense.style.backgroundColor="#5B4C49";
+		if (mana_joueur>24){
+			Spe_Disponible=true;
+			Bouton_special.style.backgroundColor="#DB3613";
+		}
+		vie_joueur -= Math.round(atq_monstre*0.75);
+		Hp_perso1.innerHTML= vie_joueur;
+		Boite_dialogue.innerHTML="Vous vous protégez, le monstre vous inflige " + Math.round(atq_monstre*0.75) + " points de dégâts";
+		setTimeout(Condition_Victoire, 3000);
+		setTimeout(Condition_Defaite, 3000);
+		if (poison>0){
+			vie_monstre -= dgt_poison;
+		}
+	}
+}
+
+function Special(){
+	if (Spe_Disponible==true){
+		Atq_Disponible=true;
+		Bouton_attaquer.style.backgroundColor="#DB3613";
+		Def_Disponible=true;
+		Bouton_defense.style.backgroundColor="#DB3613";
+		Spe_Disponible=false;
+		Bouton_special.style.backgroundColor="#5B4C49";
+		if (mana_joueur>24){
+			poison=3;
+			mana_joueur -= cout_mana;
+			Mana_perso1.innerHTML=mana_joueur;
+		}
+	}
+}
+
+function Check_Poison(){
+	if (poison>0){
+		vie_monstre -= dgt_poison;
+		InfobulleVie.innerHTML=vie_monstre + "HP";
+		poison -=1;
+		Boite_dialogue.innerHTML="le monstre est empoisonné, il subit " + dgt_poison + " points de dégâts";
+	}
+}
+
+function Msg_atq(){
+	setTimeout(Attaquer, 0);
+	setTimeout(Atq_Monstre, 1500);
+	setTimeout(Check_Poison, 3000);
+	setTimeout(Condition_Victoire, 3000);
+	setTimeout(Condition_Defaite, 3000);
+
+}
+
+function Msg_spe(){
+	if (mana_joueur>24){
+		setTimeout(Special, 0);
+		setTimeout(Check_Poison, 1500);
+		setTimeout(Atq_Monstre, 3000);
+		setTimeout(Condition_Victoire, 3000);
+		setTimeout(Condition_Defaite, 3000);
+	}
+}
+
+function Msg_def(){
+	setTimeout(Defense, 0);
+	setTimeout(Check_Poison, 3000);
+	setTimeout(Condition_Victoire, 3000);
+	setTimeout(Condition_Defaite, 3000);
+}
+
+
+
+Bouton_attaquer.addEventListener("click", Msg_atq);
+Bouton_defense.addEventListener("click", Msg_def);
+Bouton_special.addEventListener("click", Msg_spe);
